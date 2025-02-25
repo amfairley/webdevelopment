40db38d2727cb0db45fc2b0d6270a91553c2fb9e
amfairley <adamfairley1990@gmail.com>
2025-02-25 12:20:52 +0000

    js: add highlight js light dark mode functionality

--- static/js/base.js
+++ static/js/base.js
@@ -5,16 +5,21 @@ document.addEventListener("DOMContentLoaded", function () {
         "day-mode-toggle-dropdown"
     );
 
+    // Highlight JS CDN links
+    const hightlightJsCDN = document.getElementById('highlightjscdn');
+
     // Set initial selected mode
     function setSelectedMode(mode) {
         if (mode === "<i class='fa-solid fa-sun'></i>") {
             dayButton.classList.add("active");
             nightButton.classList.remove("active");
             modeToggleButton.html = "<i class='fa-solid fa-sun'></i>";
+            hightlightJsCDN.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css');
         } else {
             nightButton.classList.add("active");
             dayButton.classList.remove("active");
             modeToggleButton.html = "<i class='fa-solid fa-moon'></i>";
+            hightlightJsCDN.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/atom-one-dark.min.css');
         }
     }
 
@@ -23,19 +28,23 @@ document.addEventListener("DOMContentLoaded", function () {
         document.body.classList.remove("night-mode");
         document.body.classList.add("day-mode");
         setSelectedMode("<i class='fa-solid fa-sun'></i>");
+        hightlightJsCDN.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css');
     });
 
     nightButton.addEventListener("click", function () {
         document.body.classList.remove("day-mode");
         document.body.classList.add("night-mode");
         setSelectedMode("<i class='fa-solid fa-moon'></i>");
+        hightlightJsCDN.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/atom-one-dark.min.css');
     });
 
     // Initialize with the default selected mode
     if (document.body.classList.contains("night-mode")) {
         setSelectedMode("<i class='fa-solid fa-moon'></i>");
+        hightlightJsCDN.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/atom-one-dark.min.css');
     } else {
         setSelectedMode("<i class='fa-solid fa-sun'></i>");
+        hightlightJsCDN.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/default.min.css');
     }
 
     // Make navigation bar sticky when scrolled down
