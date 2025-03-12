bc1712a0c8a69acafff90624cf2dd1b071c22d88
amfairley <adamfairley1990@gmail.com>
2025-03-12 17:53:22 +0000

    HTML: add introducing elements page

--- HTML/urls.py
+++ HTML/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.HTML_home, name="HTML_home"),
+    path(
+        'introducing_elements/',
+        views.HTML_introducing_elements,
+        name="HTML_introducing_elements"
+    )
 ]
--- HTML/views.py
+++ HTML/views.py
@@ -12,3 +12,15 @@ def HTML_home(request):
         'HTML/home/home.html',
         context
     )
+
+
+def HTML_introducing_elements(request):
+    '''Return the HTML introducing elements page'''
+    context = {
+        'MEDIA_URL': settings.MEDIA_URL,
+    }
+    return render(
+        request,
+        'HTML/introducing_elements/introducing_elements.html',
+        context
+    )
