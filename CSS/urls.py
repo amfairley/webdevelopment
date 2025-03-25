34ed1ba3d6849cc28bc5f226e46f2d6543868f42
amfairley <adamfairley1990@gmail.com>
2025-03-25 11:21:30 +0000

    CSS: update views and urls for display and positioning

--- CSS/urls.py
+++ CSS/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.CSS_home, name="CSS_home"),
+    path('selectors/', views.CSS_selectors, name="CSS_selectors"),
+    path('box-model/', views.CSS_box_model, name="CSS_box_model"),
+    path(
+        'display-and-positioning/',
+        views.CSS_display_and_positioning,
+        name="CSS_display_and_positioning"
+    ),
 ]
--- CSS/views.py
+++ CSS/views.py
@@ -12,3 +12,39 @@ def CSS_home(request):
         'CSS/home/home.html',
         context
     )
+
+
+def CSS_selectors(request):
+    '''Return the CSS selectors page'''
+    context = {
+        'MEDIA_URL': settings.MEDIA_URL,
+    }
+    return render(
+        request,
+        'CSS/selectors/selectors.html',
+        context
+    )
+
+
+def CSS_box_model(request):
+    '''Return the CSS box model page'''
+    context = {
+        'MEDIA_URL': settings.MEDIA_URL,
+    }
+    return render(
+        request,
+        'CSS/box_model/the_css_box_model.html',
+        context
+    )
+
+
+def CSS_display_and_positioning(request):
+    '''Return the CSS display and positioning page'''
+    context = {
+        'MEDIA_URL': settings.MEDIA_URL,
+    }
+    return render(
+        request,
+        'CSS/display_and_positioning/display_and_positioning.html',
+        context
+    )
