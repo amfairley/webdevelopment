ed131798f316cf0aaff8e4ca6ea75cbb8b70dd7e
amfairley <adamfairley1990@gmail.com>
2025-04-23 17:58:17 +0100

    INTRO TO FULL STACK: add GitHub page and links

--- intro_to_full_stack/urls.py
+++ intro_to_full_stack/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
         views.full_stack_programming_and_the_web,
         name="full_stack_programming_and_the_web"
     ),
+    path(
+        'github/',
+        views.full_stack_github,
+        name="full_stack_github"
+    ),
 ]
--- intro_to_full_stack/views.py
+++ intro_to_full_stack/views.py
@@ -3,12 +3,24 @@ from django.conf import settings
 
 
 def full_stack_programming_and_the_web(request):
-    '''Return the introduction to full stack page'''
+    '''Return the introduction to full stack progamming and the web page'''
     context = {
         'MEDIA_URL': settings.MEDIA_URL,
     }
     return render(
         request,
         'intro_to_full_stack/programming_and_the_web/programming_and_the_web.html',
+        context
+    )
+
+
+def full_stack_github(request):
+    '''Return the introduction to full stack github page'''
+    context = {
+        'MEDIA_URL': settings.MEDIA_URL,
+    }
+    return render(
+        request,
+        'intro_to_full_stack/github/github.html',
         context
     )
