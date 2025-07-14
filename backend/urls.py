5b88dac95d3cfc05801741def47c2b5975227f40
amfairley <adamfairley1990@gmail.com>
2025-07-14 05:33:25 +0100

    Backend: add links and page for designing a database

--- backend/urls.py
+++ backend/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.backend_home, name="backend_home"),
     path('databases/', views.backend_databases, name="backend_databases"),
+    path(
+        'designing_a_database/',
+        views.backend_designing_a_database,
+        name="backend_designing_a_database"
+    ),
 ]
--- backend/views.py
+++ backend/views.py
@@ -14,11 +14,22 @@ def backend_home(request):
 
 
 def backend_databases(request):
-    '''Return the realtional and non-relational databases homepage'''
+    '''Return the realtional and non-relational databases page'''
     context = {
     }
     return render(
         request,
         'backend/databases/databases.html',
+        context
+    )
+
+
+def backend_designing_a_database(request):
+    '''Return the designing a database page'''
+    context = {
+    }
+    return render(
+        request,
+        'backend/designing_a_database/designing_a_database.html',
         context
     )
